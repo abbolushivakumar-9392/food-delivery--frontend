fbaa9594ed2d0f4fb0ff39c3aee1d9e2e0b906a3
abbolushivakumar-9392 <abbolushivakumar@gmail.com>
2025-03-20 18:34:33 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Home() {
+  return (
+    <div>
+      <h1 align ="center" >WELCOME TO THE FOOD DELIVARY APP</h1>
+      <br></br>
+      <center><img src="images.png"/></center>
+    </div>
+  )
+}
+
+export default Home
